9d69fc99b39debbb877ed83e591f8f4177b86ec4
Colin Zelin <Colin@Colin-Zelins-MacBook-Pro.local>
2014-01-24 10:07:08 -0500

    Initial Commit

---
+++ test1pgm1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test1pgm1
+//
+//  Created by Colin Zelin on 1/24/14.
+//  Copyright (c) 2014 Colin Zelin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ test1pgm1/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  test1pgm1
+//
+//  Created by Colin Zelin on 1/24/14.
+//  Copyright (c) 2014 Colin Zelin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
